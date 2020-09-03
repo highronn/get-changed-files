733871702ad4b183b3282205e1f166451a0fd4ce
rmurat <rony.murat@gmail.com>
2020-09-03 20:02:29 +0200

    test #2

--- dist/index.js
+++ dist/index.js
@@ -3542,8 +3542,8 @@ function run() {
                         "Please submit an issue on this action's GitHub repo if you believe this in correct.");
             }
             // Log the base and head commits
-            core.info(`Base commit: ${base}`);
-            core.info(`Head commit: ${head}`);
+            core.info(`[-] Base commit: ${base}`);
+            core.info(`[-] Head commit: ${head}`);
             // Ensure that the base and head properties are set on the payload.
             if (!base || !head) {
                 core.setFailed(`The base and head commits are missing from the payload for this ${github_1.context.eventName} event. ` +
